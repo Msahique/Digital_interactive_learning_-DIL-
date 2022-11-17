32cda2647a4ee73580f723fbe9017f569d2f736b
msahqiue <msahique@apcogsys.net>
2022-11-17 10:31:13 +0530

    inital commit

---
+++ qa_list.js
@@ -0,0 +1,42 @@
+var qa_list={
+  "item": [
+    {
+      "language": "english",
+      "question": [
+        {
+          "typ": "img",
+          "src": "Picture1.png",
+          "des": "The above image is one of the most widely used picture to demonstrate image compression",
+          "ques": "Identify the person in the above image optional answers are",
+          "opt": [
+            {
+              "root": "a",
+              "alias": [
+                "s",
+                "d"
+              ],
+              "iconfile": ""
+            },
+            {
+              "root": "z",
+              "alias": [
+                "x",
+                "c"
+              ],
+              "iconfile": ""
+            }
+          ],
+          "correct_ans": [
+            "0",
+            "1",
+            "0"
+          ],
+          "user_ans": [
+            "0",
+            "0"
+          ]
+        }
+      ]
+    }
+  ]
+}
